04d7c22b07702a5ca98016240fcf3abd97956bfa
GG1NET ✔️嘛✔️ <60385397+fastergo@users.noreply.github.com>
2022-08-04 17:34:42 +0700

    Add files via upload

---
+++ js/JV7CRR9FCT.js
@@ -0,0 +1,22 @@
+cd C:\scrcpy
+scrcpy -m1080 -b15m
+scrcpy -m1080 -b15m
+scrcpy -m1920 -b5m no delay
+scrcpy -m1920 -b10m no delay
+scrcpy -m2400 -b10m
+
+Do the best and let God do the rest :)
+#MLBBIDCreator #RRQDahlanSalto #MobileLegends
+
+Link donate : https://sociabuzz.com/dahlansalto/donate
+
+Jangan lupa yang baru join Like Share And Subscribe
+
+Follow Instagram @yagamiesports :D
+
+Business & partnership : 
+partnership@-
+Let's together break the limit!
+
+
+Thanks for coming!
